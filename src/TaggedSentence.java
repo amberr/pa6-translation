6bc1eda1cd4f3d818ecef253dd28532d6a914fbe
guthriec <guthriec@stanford.edu>
2014-02-23 23:23:22 -0800

    method to print to string, quick test in main

--- src/TaggedSentence.java
+++ src/TaggedSentence.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.ArrayList;
 
 /*
  * Initialize this with something like:
- * taggedSentence = new TaggedSentence(wordArray, POSArray);
+ * TaggedSentence taggedSentence = new TaggedSentence(wordArray, POSArray);
  * where wordArray and POSArray are arrays of translated words and corresponding
  * tagged parts of speech.
  */
@@ -26,15 +27,33 @@ public class TaggedSentence{
 			sentence.add(new TaggedWord(wordArr[i], posArr[i]));
 		}
 	}
+	
 	/*
 	 * Takes a rule of the form (part of speech, part of speech) and applies it
 	 * to the entire sentence, swapping any adjacent words matching the rule.
 	 */
-	public void SwapAllAdjacent(String pos1, String pos2) {
+	public void swapAllAdjacent(String pos1, String pos2) {
 		for (int i=0; i < sentence.size()-1; i++) {
 			if (sentence.get(i).pos == pos1 && sentence.get(i+1).pos == pos2) {
 				Collections.swap(sentence, i, i+1);
 			}
 		}
+	}
+	
+	public String sentence() {
+		StringBuilder sb = new StringBuilder();
+		for (TaggedWord tWord : sentence) {
+			sb.append(tWord.word);
+			sb.append(" ");
+		}
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		String[] wordArray = new String[] {"Hello", "I", "am", "a", "sentence"};
+		String[] POSArray = new String[] {"A", "B", "C", "A", "B"};
+		TaggedSentence taggedSentence = new TaggedSentence(wordArray, POSArray);
+		taggedSentence.swapAllAdjacent("A", "B");
+		System.out.println(taggedSentence.sentence());
 	}
 }
